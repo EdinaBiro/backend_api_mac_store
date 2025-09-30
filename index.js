bebf9f63a63bc069ab5abae28e099c79fd6a4dce
Edina Biro <edina.biro@reea.net>
2025-09-30 14:40:28 +0300

    Created a test api

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 
 const express = require('express');
 
+const helloRoute = require('./routes/hello');
+
 //define the port number the server will listen on
 const PORT = 3000;
 
 //create an instance of an express application for the starting point
 const app = express();
+
+//middleware to register routes or to mount routes
+
+app.use(helloRoute);
 
 //start the server and listen on the specified port
 app.listen(PORT, "0.0.0.0" ,function(){
