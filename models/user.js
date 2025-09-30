96497a343381e6c6be9644efa8dd1add73cf488b
Edina Biro <edina.biro@reea.net>
2025-09-30 15:35:04 +0300

    Define user schema

---
+++ models/user.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    fullName:{
+        type: String,
+        required: true,
+        trim: true,
+    },
+
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    state:{
+       type: String,
+        required: true,
+    },
+    city:{
+       type: String,
+        default: "",
+    },
+    locality: {
+        type: String,
+        default: "",
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
