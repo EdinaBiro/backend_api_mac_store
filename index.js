979acdde2f0b9477075aa602f4f82937bda7e215
Edina Biro <edina.biro@reea.net>
2025-09-30 15:12:57 +0300

    Connected and resgitered a mongo db for the project

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-
+require('dotenv').config();
 const express = require('express');
-
 const helloRoute = require('./routes/hello');
+const mongoose = require('mongoose');
 
 //define the port number the server will listen on
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 //create an instance of an express application for the starting point
 const app = express();
-
+const DB = process.env.MONGO_URI;
 //middleware to register routes or to mount routes
 
 app.use(helloRoute);
+
+
+mongoose.connect(DB).then(()=>{
+    console.log('MongoDB Connected');
+});
+
 
 //start the server and listen on the specified port
 app.listen(PORT, "0.0.0.0" ,function(){
