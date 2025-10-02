c0e474fef3cbc644c8d82c0d4dd64d17f7ab1859
Edina Biro <edina.biro@reea.net>
2025-10-02 16:09:21 +0300

    Add route for retrieving subcategories by category name

--- routes/sub_category.js
+++ routes/sub_category.js
@@ -13,4 +13,20 @@ subCategoryRouter.post('/api/subcategories', async(req,res)=>{
     }
 });
 
+subCategoryRouter.get('/api/category/:categoryName/subcategories', async(req,res)=>{
+    try{
+    const {categoryName} = req.params;
+
+    const subcategories = await subCategory.find({categoryName: categoryName});
+
+    if(!subcategories|| subcategories.length == 0){
+        return res.status(404).json({msg:"subcategories not found"});
+    }else{
+        return res.status(200).json(subcategories);
+    }
+}catch(e){
+    res.status(500).json({error: e.message});
+}
+});
+
 module.exports = subCategoryRouter;
