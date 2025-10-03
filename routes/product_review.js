2fd9647b2428620f3a9f0344bd92d06a5f7c859b
Edina Biro <edina.biro@reea.net>
2025-10-03 11:12:12 +0300

    Add product review route and integrate it into the main application

--- routes/product_review.js
+++ routes/product_review.js
@@ -2,14 +2,16 @@ const express = require('express');
 const ProductReview = require('../models/product_review');
 
 const ProductReviewRouter = express.Router();
-
-ProductReviewRouter.post('/api/product-review', async(requestAnimationFrame,res)=>{
+    
+ProductReviewRouter.post('/api/product-review', async(req,res)=>{
     try{
-    const {buyerId, email, fullName, productId,rating, review} = requestAnimationFrame.body;
+    const {buyerId, email, fullName, productId,rating, review} = req.body;
     const reviews = new ProductReview({buyerId, email, fullName, productId,rating, review});
     await reviews.save();
     return res.status(201).send(reviews);
     }catch(e){
-        res.status(500).json({error: e.messsage});
+        res.status(500).json({error: e.message});
     }
 });
+
+module.exports = ProductReviewRouter;
