6711a12515d08aa6d6b657fff4b747d45531d1ac
Edina Biro <edina.biro@reea.net>
2025-10-03 09:10:38 +0300

    Add product model with schema definition for products

---
+++ models/products.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const subCategory = require('./sub_categories');
+
+const productSchema = mongoose.Schema({
+    productName:{
+        type: String,
+        trim: true,
+        required: true,
+    },
+    productPrice:{
+        type: Number,
+        required: true,
+    },
+    quantity:{
+        type: Number,
+        required: true,
+    },
+    description:{
+        type: String,
+        required: true,
+    },
+    category:{
+        type: String,
+        required: true,
+    },
+    subCategory:{
+        type: String,
+        required: true,
+    },
+    images:[
+        {
+            type: String,
+            required: true,
+
+        }
+    ],
+    popular:{
+        type: Boolean,
+        default: true,
+    },
+    reccomend:{
+        type: Boolean,
+        default: false,
+    },
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
